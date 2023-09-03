5a83d9b53bd5f44448b8cb9ddb1897295637796d
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-09-04 00:14:18 +0100

    Issue 9: Complete the 'saveThePrisoner' function below.

---
+++ HackeRank/Prepare/Algorithms/Implementation/Save the Prisoner!/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+Console.WriteLine("Enter the number of test cases:");
+int t = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < t; i++)
+{
+    Console.WriteLine($"Test Case {i + 1}");
+            
+    Console.WriteLine("Enter the number of prisoners:");
+    int n = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Enter the number of sweets:");
+    int m = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Enter the chair number to start passing out treats at:");
+    int s = int.Parse(Console.ReadLine());
+
+    int result = SaveThePrisoner(n, m, s);
+    Console.WriteLine($"Prisoner to warn: {result}");
+}
+
+static int SaveThePrisoner(int n, int m, int s)
+{
+    int prisonerToWarn = (s + m - 2) % n + 1;
+        
+    return prisonerToWarn;
+}
