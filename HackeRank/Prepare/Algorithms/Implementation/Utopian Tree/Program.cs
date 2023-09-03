04aee3b04f193ee7cb20f3c32a53f8b17a7425e5
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-09-03 23:06:08 +0100

    Issue 7: Complete the 'utopianTree' function below.

---
+++ HackeRank/Prepare/Algorithms/Implementation/Utopian Tree/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+Console.WriteLine("Enter the number of test cases:");
+int t = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < t; i++)
+{
+    Console.WriteLine($"Enter the number of cycles for test case {i + 1}:");
+    int n = int.Parse(Console.ReadLine());
+
+    int result = UtopianTree(n);
+    Console.WriteLine($"Height after {n} cycles: {result}");
+}
+
+static int UtopianTree(int n)
+{
+    int height = 1; 
+
+    for(int i = 0; i < n; i++)
+    {
+        if(i % 2 == 0)
+        {
+            height *= 2;
+        }
+        
+        else
+        {
+            height += 1;
+        }
+    }
+
+    return height;
+}    
