d7e48852044c3ecdecac299fa591e67ad18cb623
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-09-06 06:32:59 +0100

    Issue 13: Complete the 'findDigits' function below.

---
+++ HackeRank/Prepare/Algorithms/Implementation/Find Digits/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Enter the number of test cases:");
+int testCase = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < testCase; i++)
+{
+    Console.WriteLine("Enter the integer to analyze:");
+    int userInput = int.Parse(Console.ReadLine());
+
+    int result = FindDigits(userInput);
+
+    Console.WriteLine("The number of divisors in the integer: " + result);
+}
+
+static int FindDigits(int number)
+{
+    int originalNumber = number;
+    int count = 0;
+
+    while(number > 0)
+    {
+        int digit = number % 10; 
+        if(digit != 0 && originalNumber % digit == 0)
+        {
+            count++;
+        }
+        number /= 10; 
+    }
+
+    return count;
+}
