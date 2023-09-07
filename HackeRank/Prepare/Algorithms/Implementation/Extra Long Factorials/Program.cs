255cc4fc8d380e5755b2d76433dca7c1e9a9c731
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-09-07 11:12:25 +0100

    Issue 14: Complete the 'extraLongFactorials' function below.

---
+++ HackeRank/Prepare/Algorithms/Implementation/Extra Long Factorials/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Numerics;
+
+Console.WriteLine("Enter an integer to calculate its factorial:");
+int userInput = int.Parse(Console.ReadLine());
+
+ExtraLongFactorials(userInput);
+
+static void ExtraLongFactorials(int number)
+{
+    BigInteger result = 1;
+
+    for(int i = 2; i <= number; i++)
+    {
+        result *= i;
+    }
+
+    Console.WriteLine(result);
+}
