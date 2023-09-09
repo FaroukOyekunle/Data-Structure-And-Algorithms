3790d2d1cc824bf55482ccdea591451ae1506f16
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-09-09 18:17:00 +0100

    Issue 16: Complete the 'squares' function below.

---
+++ HackeRank/Prepare/Algorithms/Implementation/Sherlock and Squares/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+Console.WriteLine("Enter the number of test cases:");
+int q = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < q; i++)
+{
+    Console.WriteLine($"Test Case {i + 1}:");
+
+    Console.Write("Enter the lower range boundary (a): ");
+    int a = int.Parse(Console.ReadLine());
+
+    Console.Write("Enter the upper range boundary (b): ");
+    int b = int.Parse(Console.ReadLine());
+
+    int result = CountSquareIntegers(a, b);
+    Console.WriteLine($"Number of square integers in the range [{a}, {b}]: {result}");
+    Console.WriteLine();
+}
+
+static int CountSquareIntegers(int a, int b)
+{
+    int count = 0;
+    int current = 1;
+
+    while(true)
+    {
+        int square = current * current;
+
+        if(square > b)
+        {
+            break;
+        }
+
+        if(square >= a)
+        {
+            count++;
+        }
+
+        current++;
+    }
+
+    return count;
+}
