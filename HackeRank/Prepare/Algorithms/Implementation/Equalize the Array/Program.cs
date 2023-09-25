cb7dabbc844efaa614db452a09e6630a807c434b
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-09-25 13:02:50 +0100

    Issue 22: Complete the 'equalizeArray' function below. The function is expected to return an INTEGER. The function accepts INTEGER_ARRAY arr as parameter.

---
+++ HackeRank/Prepare/Algorithms/Implementation/Equalize the Array/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+Console.WriteLine("Welcome to the Array Equalization Program!");
+
+Console.Write("Enter the number of elements in the array: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the array elements separated by spaces: ");
+List<int> arr = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+int result = EqualizeArray(arr);
+Console.WriteLine($"Minimum number of deletions required is: {result}");
+
+static int EqualizeArray(List<int> arr)
+{
+    Dictionary<int, int> frequencyMap = new Dictionary<int, int>();
+
+    foreach (int num in arr)
+    {
+        if (frequencyMap.ContainsKey(num))
+        {
+            frequencyMap[num]++;
+        }
+            
+        else
+        {
+            frequencyMap[num] = 1;
+        }
+    }
+
+    int maximumFrequency = frequencyMap.Values.Max();
+
+    int minimumDeletions = arr.Count - maximumFrequency;
+
+    return minimumDeletions;
+}   
