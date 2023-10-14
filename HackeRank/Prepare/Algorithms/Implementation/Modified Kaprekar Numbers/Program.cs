deb553e9bd212629f68b5c6b8c02a223fe734aba
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-10-14 18:13:01 +0100

    Issue 28: Complete the 'kaprekarNumbers' function below. 1, 9, 45, 55, and 99 are the modified Kaprekar Numbers in the given range.

---
+++ HackeRank/Prepare/Algorithms/Implementation/Modified Kaprekar Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Welcome to the Modified Kaprekar Number Finder!");
+Console.Write("Enter the lower limit (p): ");
+int p = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter the upper limit (q): ");
+int q = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Modified Kaprekar Numbers in the given range:");
+KaprekarNumbers(p, q);
+
+static void KaprekarNumbers(int p, int q)
+{
+    bool found = false;
+    for (int number = p; number <= q; number++)
+    {
+        long square = (long)number * number;
+        string squareNumber = square.ToString();
+        int d = number.ToString().Length;
+
+        int right = int.Parse(squareNumber.Substring(squareNumber.Length - d));
+        int left = (squareNumber.Length > d) ? int.Parse(squareNumber.Substring(0, squareNumber.Length - d)) : 0;
+
+        if (left + right == number)
+        {
+            Console.Write(number + " ");
+            found = true;
+        }
+    }
+
+    if (!found)
+        Console.WriteLine("INVALID RANGE");
+    else
+        Console.WriteLine();
+}    
